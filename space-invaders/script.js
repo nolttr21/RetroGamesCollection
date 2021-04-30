cf1a63a1c667e3893ca4bd50b3ec74c037e82b24
Julio Pochet Edmead <39777664+jpochetedmead@users.noreply.github.com>
2021-04-30 00:52:05 -0400

    Created Space-Invaders folder, html, css and js files, updated the html and css file of the game.

---
+++ space-invaders/script.js
@@ -0,0 +1,18 @@
+/**************************
+      * Space Invaders *
+ **************************/
+/*
+1. As a Player, I want to press the arrow keys so I can move the space ship left and right.  (3)(Done... can be improved.)
+2. As a Player, I want to see the enemies, so I can aim at them and see their movements (3)(Done... can be improved.)
+3. As a Player, I want to press the up key, so I can see the bullet and shoot the enemies and see when enemies shoot at me (8)(Done with player, working on the enemies...)
+4. As a Player, I want to see the enemies blow up, so I can tell that they are dead (5)(Done...could be improved by adding explosion image and/or sound.)
+5. As a Player, I want to see my lives and know when I die , so I can see when I’m going to die and when to restart (2)
+6. As a player I want to see the score, so I can see how well I’m doing (1)(Done.)
+7. As a Player, I want to see an end screen , so I can tell that I beat the level (1)(Done.)
+8. As a Player, I want to press a button or the "R"/"r" key, so I can restart the game (1)(Done.)
+Controls:
+Left Arrow = Turn Left
+Right Arrow = Turn Right
+Up Arrow / Space Bar = Shoot
+R / r  = Restart Game
+*/
